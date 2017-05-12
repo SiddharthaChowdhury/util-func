3cc622cea263bbeffec926313fffae0492a6b54c
Siddhartha <siddharthac6@gmail.com>
2017-05-12 22:43:57 +0530

    (UniqID and randomStr): Undefined issues fixed

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 // UTIL.js
 module.exports = {
-	uniqueID: function(length){
-		const n = length || 38;
+	uniqueID: function(){
+		const n = length || 15;
 		const unixTstamp = Math.round(new Date().getTime()/1000);
-		const chars = Math.random().toString(36).slice(2)
-		var result = '';
-	    for (var i = n; i > 0; --i) result += chars[Math.round(Math.random() * (chars.n - 1))];
-	    return result;
+		const possible = unixTstamp.toString();
+		var text = '';
+	    for( var i=0; i<n; i++ )  text += possible.charAt(Math.floor(Math.random() * possible.length));
+	    return text+unixTstamp;
 	},
 	randomString128bit: function(){ // returns 128 bit string
 		var md5 = require('js-md5'); // dependency
 		const unixTstamp = Math.round(new Date().getTime()/1000);
 		const random = Math.random().toString(36).slice(2);
-		return md5(random+unixTstamp)
+		return md5(randomunixTstamp)
 	},
     randomString: function(length){
     	var n = length || 38;
 	    if(!n){
-			var min = 8;
-			var max = 12;
-			n = Math.floor(Math.random()*(max-min+1)+min);
+	        var min = 8;
+	        var max = 12;
+	        n = Math.floor(Math.random()*(max-min+1)+min);
 	    }
-	    var result = '';
-	    var chars = '012345679abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	    for (var i = n; i > 0; --i) result += chars[Math.round(Math.random() * (chars.n - 1))];
-	    return result;
+      	var text = "";
+    	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    	for( var i=0; i<n; i++ )
+        	text += possible.charAt(Math.floor(Math.random() * possible.length));
+    	return text;
 	},
+  
 	getSlug: function(str){
 		return str.trim().toLowerCase().replace(/[^a-z0-9-]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/g, '');
 	},
