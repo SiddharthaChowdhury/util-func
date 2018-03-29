a621317919525d1b44efc52d250fe8b052327b64
SIDDHARTHA CHOWDHURY <siddhartha.chowdhury@lighting.com>
2018-03-29 13:54:53 +0530

    (MOdified): getRandomString and added date fns

--- README.md
+++ README.md
@@ -23,8 +23,16 @@ Bunch of widely used functions in most common public projects
 	randomString128bit: function(){ 
 		// returns 128 bit string
 	},
-    randomString: function(length){
-	    // get random string of size `n` passed as argument || default 38
+    randomString: function(Flag){
+	    /*
+	    Returns random string of specified length
+	    ~~~~~~~~~~~~~~~ SIGNATURE ~~~~~~~~~~~~~~~~~
+	    INPUT: Optional
+	    	randomString({min:4, max:8}).length // Can pass object with min, max value
+ 			randomString(8).length // Can pass fixed lingth value as Number
+			randomString().length // default returns between 8 and 20
+		RETURNS: A random string of specified length
+		*/
 	},
 	getSlug: function(str){
 		// Get string converted to slug
@@ -38,4 +46,33 @@ Bunch of widely used functions in most common public projects
 	getDateTime: function(){
 		// Get current local date and time [ format: dd/mm/yyyy, hh:mm:ss ]
 	},
+	getYesterday: function(daysBack = 1, ofDay = new Date()){
+		/*
+		This function is responsible for get "yesterday's" date dynamically if no parameters passed
+		"daysBack" is how many days back of "ofDay"
+		By default it gets date "1" day before "current date" i.e. today - will return yesterday   
+		~~~~~~~~~~~~~~~ SIGNATURE ~~~~~~~~~~~~~~~~~
+		INPUT = (optional) "daysBack" is how many days back of "ofDay"- format "yyyy-mm-dd"
+		RETURN = "date" format: "yyyy-mm-dd", based on input  
+		*/
+	},
+	datesBetween: function(from, to){
+		/*
+		This function gives output as an ARRAY of all dates between two dates provided as input   
+		~~~~~~~~~~~~~~~ SIGNATURE ~~~~~~~~~~~~~~~~~
+		INPUT = "from", "to" in format 'mm/dd/yyyy'
+		RETURN = ARRAY of dates
+		*/
+	},
+	paginationOffset: function(pageNumber, size = 10){
+		/* 
+		Useful for pagination
+		INPUT: current page number
+		RETURN:{
+	      		"from": Number, 	// Where to start from
+	      		"to": Number,		// Where to stop
+	      		"size": pageSize	// Number of records
+	  		} 
+		*/
+	}
 
--- index.js
+++ index.js
@@ -1,46 +1,107 @@
 // UTIL.js
 module.exports = {
-	uniqueID: function(){
-		const n = length || 15;
-		const unixTstamp = Math.round(new Date().getTime()/1000);
-		const possible = unixTstamp.toString();
-		var text = '';
-	    for( var i=0; i<n; i++ )  text += possible.charAt(Math.floor(Math.random() * possible.length));
-	    return text+unixTstamp;
-	},
-	randomString128bit: function(){ // returns 128 bit string
-		var md5 = require('js-md5'); // dependency
-		const unixTstamp = Math.round(new Date().getTime()/1000);
-		const random = Math.random().toString(36).slice(2);
-		return md5(randomunixTstamp)
-	},
-    randomString: function(length){
-    	var n = length || 38;
-	    if(!n){
-	        var min = 8;
-	        var max = 12;
-	        n = Math.floor(Math.random()*(max-min+1)+min);
-	    }
-      	var text = "";
-    	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    	for( var i=0; i<n; i++ )
-        	text += possible.charAt(Math.floor(Math.random() * possible.length));
-    	return text;
-	},
-  
-	getSlug: function(str){
-		return str.trim().toLowerCase().replace(/[^a-z0-9-]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/g, '');
-	},
-	getDate: function(){
-		return new Date().toLocaleDateString();
-	},
-	getTime: function(){
-		return new Date().toLocaleTimeString();
-	}, 
-	getDateTime: function(){
-		return new Date().toLocaleString();
-	},
-	getUnixTimeStamp: function(){
-		return Date.now();
-	}
+    uniqueID: function() {
+        const n = length || 15;
+        const unixTstamp = Math.round(new Date().getTime() / 1000);
+        const possible = unixTstamp.toString();
+        var text = '';
+        for (var i = 0; i < n; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text + unixTstamp;
+    },
+    randomString128bit: function() { // returns 128 bit string
+        var md5 = require('js-md5'); // dependency
+        const unixTstamp = Math.round(new Date().getTime() / 1000);
+        const random = Math.random().toString(36).slice(2);
+        return md5(randomunixTstamp)
+    },
+    randomString: function(flag) {
+        var n, min = null,
+            max = null;
+        if (!flag) {
+            min = 8;
+            max = 20;
+            n = Math.floor(Math.random() * (max - min + 1)) + min
+        } else if (typeof flag === "object") {
+            min = flag.min;
+            max = flag.max;
+            n = Math.floor(Math.random() * (max - min + 1)) + min
+        } else if (typeof flag === "number" || typeof flag === "string") {
+            n = parseInt(flag)
+        }
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (var i = 0; i < n; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
+    },
+
+    getSlug: function(str) {
+        return str.trim().toLowerCase().replace(/[^a-z0-9-]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/g, '');
+    },
+    getDate: function() {
+        return new Date().toLocaleDateString();
+    },
+    getTime: function() {
+        return new Date().toLocaleTimeString();
+    },
+    getDateTime: function() {
+        return new Date().toLocaleString();
+    },
+    getUnixTimeStamp: function() {
+        return Date.now();
+    },
+    // This function is responsible for get "yesterday's" date dynamically if no parameters passed
+    // "daysBack" is how many days back of "ofDay"
+    // By default it gets date "1" day before "current date" i.e. today - will return yesterday   
+    // ~~~~~~~~~~~~~~~ SIGNATURE ~~~~~~~~~~~~~~~~~
+    // INPUT = (optional) "daysBack" is how many days back of "ofDay"- format "yyyy-mm-dd"
+    // DEPENDENCY = none;
+    // RETURN = "date" format: "yyyy-mm-dd", based on input  
+    getYesterday: function(daysBack = 1, ofDay = new Date()) {
+        var dt, day = "",
+            month = "",
+            year = "";
+        dt = ofDay;
+
+        var yd = new Date(dt);
+        yd.setDate(yd.getDate() - daysBack);
+        day = yd.getDate().toString();
+        day = day.length == 2 ? day : "0" + day;
+
+        month = (yd.getMonth() + 1).toString();
+        month = month.length == 2 ? month : "0" + month;
+
+        year = yd.getFullYear()
+
+        // date format: yyyy-mm-dd
+        var yesterday = year + "-" + month + "-" + day;
+        return yesterday;
+    },
+
+    datesBetween: function(frm, to) {
+        // "frm", "to" in format 'mm/dd/yyyy'
+        var start = new Date(frm);
+        var end = new Date(to);
+        var newend = end.setDate(end.getDate() + 1);
+        var end = new Date(newend);
+        var final = [];
+        while (start < end) {
+            final.push(start.getFullYear() + '-' + (start.getMonth() + 1) + '-' + start.getDate());
+            //console.log(fin); // ISO Date format
+            var newDate = start.setDate(start.getDate() + 1);
+            start = new Date(newDate);
+        }
+        return final;
+    },
+    paginationOffset: function(page, size = 10) {
+        var pageSize = size;
+        var offset = (page - 1) * (pageSize + 1);
+        var frm = offset == 0 ? 0 : (offset - 1);
+        var to = frm + pageSize;
+        return {
+            "from": frm,
+            "to": to,
+            "size": pageSize
+        }
+    }
 }
